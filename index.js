336db7ed1c447af41c187a7a5bead9705ffeb1d4
DrGooseman <jamesjquinn12@gmail.com>
2019-12-27 17:45:25 +0300

    Refactored callback hell

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 console.log("before");
-const user = getUser(1, function(user) {
-	console.log("user", user);
-	getRepositories(user.gitHubUsername, function(repos){
-		console.log("repos", repos);
-	})
-});
+getUser(1, getRepositories);
 console.log("after");
 
 function getUser(id, callback){
@@ -14,10 +9,32 @@ function getUser(id, callback){
 	}, 2000);
 }
 
-function getRepositories(username, callback) {
+function getRepositories2(username, callback) {
 	setTimeout(() => {
 		console.log("reading github...");
 		callback( ["repo1", "repo2", "repo3"]);
 	}, 2000);
 	
+}
+
+function getCommits2(repo, callback) {
+	setTimeout(() => {
+		console.log("reading repo...");
+		callback( ["commit1", "commit2", "commit3"]);
+	}, 2000);
+	
+}
+
+function getRepositories(user) {
+	console.log("user", user);
+	getRepositories2(user.gitHubUsername, getCommits)
+}
+
+function getCommits (repos){
+		getCommits2(repos[0], displayCommits);
+	}
+
+function displayCommits(commits)
+{
+	console.log(commits);
 }
