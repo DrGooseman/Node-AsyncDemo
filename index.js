f9ed16cff4b8ac9bc7fb621dc8b430ee89a84559
DrGooseman <jamesjquinn12@gmail.com>
2019-12-28 22:57:59 +0300

    Refactored, changed to async/await

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 console.log("before");
+/*
 getUser(1)
 	.then(user => getRepositories(user.gitHubUsername))
 	.then(repos => getCommits(repos[0]))
 	.then(commits => console.log("Commits",commits))
 	.catch(err => console.log("Error",err.message));
+*/	
+
+displayCommits();
 console.log("after");
+
+async function displayCommits()
+{
+try{
+	const user = await getUser(1);
+	const repos = await getRepositories(user.gitHubUsername);
+	const commits = await getCommits(repos[0]);
+	console.log("Commits", commits);
+	}
+	catch(err)
+	{
+		console.log("Error", err.message);
+	}
+}
 
 function getUser(id){
 	return new Promise((resolve, reject) => {
